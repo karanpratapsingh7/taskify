085366191436873f9cd63510f1284c79b04cf8c7
Karan Pratap Singh <karanrasmai3264@gmail.com>
2025-08-11 02:21:52 +0530

    Update conn.js

--- server/connection/conn.js
+++ server/connection/conn.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 
 const conn = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB:", error);
