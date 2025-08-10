1289fa4deba344e17fbc14fa719aaa0ca3485d5b
Karan Pratap Singh <karanrasmai3264@gmail.com>
2025-08-11 02:03:30 +0530

    Update Register.jsx

--- client/src/pages/Registeration/Register.jsx
+++ client/src/pages/Registeration/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        `${serverUrl}/api/v1/register`,
+        `${serverUrl}/register`,
         Values
       );
       setValues({
