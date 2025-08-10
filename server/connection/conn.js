e9c2f359f1acee4f2094ce701d30504647e70e1b
Karan Pratap Singh <karanrasmai3264@gmail.com>
2025-08-11 01:25:18 +0530

    Update conn.js

--- server/connection/conn.js
+++ server/connection/conn.js
@@ -1,11 +1,18 @@
+require('dotenv').config();  
+
 const mongoose = require("mongoose");
+
 const conn = async () => {
   try {
-    await mongoose.connect("mongodb+srv://karanrasmai3264:Karan2026@cluster0.lgdb6ok.mongodb.net/Taskify");
-    console.log("Connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB:", error);
   }
 };
 
 conn();
+
