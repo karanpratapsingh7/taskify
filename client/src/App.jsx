74f1fb2826073f859f3451be28dbead400a36f1c
Karan Pratap Singh <karanrasmai3264@gmail.com>
2025-08-11 02:47:45 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Register from "./pages/Registeration/Register";
-import { Routes, Route, Navigate } from "react-router-dom";  
+import { Routes, Route} from "react-router-dom";  
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
 export const serverUrl = "https://taskifyapp-1yyh.onrender.com/api/v1";
@@ -11,12 +11,8 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route
-          path="/"
-          element={
-            isLoggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
-          }
-        />
+       
+         
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
